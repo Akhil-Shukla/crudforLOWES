954f9d668e03df0c494511cd97171db55269c927
Akhil Shukla <akhil.shukla@flipkart.com>
2021-06-07 23:14:50 +0530

    testing

--- index.js
+++ index.js
@@ -7,24 +7,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/fellow', (req, res) => {
-  res.send('Hello fellow!')
-});
-
-<<<<<<< HEAD
-app.get('/madagascar', (req, res) => {
-  res.send('madagascar!')
-});
-
-
-=======
-app.get('/thisissparta', (req, res) => {
-  res.send('Hello fellow!')
-});
-
-
-
->>>>>>> 010d6e62c6a2f76c4b92cbd634ff8d4c263b6cfc
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
