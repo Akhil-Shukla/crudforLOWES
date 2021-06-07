cdd5103a097ce93836048ee7ff032db102712fb0
Akhil Shukla <akhil.shukla@flipkart.com>
2021-06-07 23:53:10 +0530

    test

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
 app.get('/stashtest', (req, res) => {
   res.send('Hello World!')
 });
+app.get('/testingfor stash', (req, res) => {
+  res.send('Hello testing for stash!')
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
